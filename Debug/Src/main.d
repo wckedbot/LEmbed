ed1549d67e8a6f5235576f25a96cce29b9fe7e0f
Shreyansh <shreyanshanand84@gmail.com>
2024-01-09 17:24:46 +0530

    Added drivers folder

---
+++ Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/shreyanshanand/STM32CubeIDE/workspace_1.14.0/LEmbed/drivers/inc/stm32f446xx.h
+C:/Users/shreyanshanand/STM32CubeIDE/workspace_1.14.0/LEmbed/drivers/inc/stm32f446xx.h:
---
+++ drivers/inc/stm32f446xx.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f446xx.h
+ *
+ *  Created on: Jan 9, 2024
+ *      Author: shreyanshanand
+ */
+
+#ifndef INC_STM32F446XX_H_
+#define INC_STM32F446XX_H_
+
+
+
+#endif /* INC_STM32F446XX_H_ */
